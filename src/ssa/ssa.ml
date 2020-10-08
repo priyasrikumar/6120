d88b9f36c39b1283c16dff9f4e4e94d0952de9a2
Goktug <gs2417@nyu.edu>
2020-10-08 02:44:45 -0400

    reworking ssa, everything works except removal of bad phi nodes

--- src/ssa/ssa.ml
+++ src/ssa/ssa.ml
@@ -2,6 +2,9 @@ open Core
 open Cfg 
 open Types
 
+type phi_tmp =
+  Phi' of dst * typ * (lbl * arg) list
+
 let update_defs var lbl defs =
   Hashtbl.update defs var ~f:(function
       | None -> let set = Hash_set.create (module String) in
@@ -35,44 +38,36 @@ let vars_of_process_defs lbl defs block =
         (dst,typ) :: acc 
     ) 
 
-let insert_nodes prog blocks df =
+let insert_nodes func_name func_args blocks df =
   let defs = Hashtbl.create (module String) in
-  let vars = List.concat_map blocks ~f:(fun (lbl,instrs) ->
+  let vars =
+    let init = List.concat_map blocks ~f:(fun (lbl,instrs) ->
       vars_of_process_defs lbl defs instrs)
-  in
-  let vars = List.fold_left prog ~init:vars ~f:(fun acc func ->
-      match func.args with
-      | None -> acc
-      | Some (args) -> List.fold_left args ~init:acc ~f:(fun acc (arg,typ) ->
-          update_defs arg func.name defs; (arg,typ) :: acc))
+    in
+    List.fold_left func_args ~init:init ~f:(fun acc (arg,typ) ->
+      update_defs arg func_name defs; (arg,typ) :: acc)
     |> List.stable_dedup
   in
-  let block_map = Hashtbl.of_alist_exn (module String) blocks in 
+  let phi_map = Hashtbl.create (module String) in
+  List.iter blocks ~f:(fun (lbl,_) ->
+      Hashtbl.add_exn phi_map ~key:lbl ~data:(Hashtbl.create (module String)));
   List.iter vars ~f:(fun (v,t) ->
-      let defs_v = Hashtbl.find_exn defs v in
-      let defs_v' = Hash_set.copy defs_v in 
-      Hash_set.iter defs_v' ~f:(fun d -> 
-          let df = Hashtbl.find_exn df d in
-          Hash_set.iter df ~f:(fun lbl ->
-              let instrs = Hashtbl.find_exn block_map lbl in      
-              let has_phi = List.fold_left instrs ~init:false ~f:(fun acc instr ->
-                  match instr with
-                  | Phi (v', _, _) when String.equal v v' -> true
-                  | _ -> acc || false)
-              in 
-              if has_phi |> not then 
-                Hashtbl.update block_map lbl ~f:(function
-                    | None -> failwith "should be unreachable"
-                    | Some (instrs) -> Phi (v, t, []) :: instrs
-                  ); 
-              Hash_set.add defs_v lbl)));
-  vars, block_map
-
-let elim_uneccesary_phis prog block_map cfg_succ =
-  List.iter prog ~f:(fun func ->
-    let lbls = traverse_cfg_pre func.name cfg_succ in 
-    let 
-  )
+    let defs_v = Hashtbl.find_exn defs v in
+    let defs_queue = Queue.create () in
+    Hash_set.iter defs_v ~f:(Queue.enqueue defs_queue);
+    while Queue.is_empty defs_queue |> not do
+      let d = Queue.dequeue_exn defs_queue in
+      let df = Hashtbl.find_exn df d in
+      Hash_set.iter df ~f:(fun block ->
+        let phi_tbl = Hashtbl.find_exn phi_map block in 
+        if Hashtbl.mem phi_tbl v |> not then
+          Hashtbl.add_exn phi_tbl ~key:v ~data:(Phi' (v,t,[]));
+          if Hash_set.mem defs_v block |> not then begin
+            Hash_set.add defs_v block;
+            Queue.enqueue defs_queue block
+          end)
+    done);
+  vars, phi_map
 
 let mk_name v i = Printf.sprintf "%s_%d" v i
 
@@ -131,6 +126,7 @@ let update_intrs stack block =
 in
 block', !stack_hist
 
+(*
 let update_phi_reads stack l_prev block =
   let process_arg dst = process_arg stack dst in
   List.map block ~f:(fun instr ->
@@ -140,51 +136,77 @@ let update_phi_reads stack l_prev block =
       let phis' = (l_prev,v') :: phis in 
       Phi (dst, typ, phis')
     | _ -> instr)
+*)
 
-let update_func_args stack args =
+(*let update_func_args stack args =
   Option.(args >>| (fun args' ->
     List.map args' ~f:(fun (arg,typ) ->
-      (process_arg stack arg,typ))))
+      (process_arg stack arg,typ))))*)
+let update_func_args stack args =
+  List.map args ~f:(fun (arg,typ) -> (process_arg stack arg,typ))
 
-let _stack_copy stack =
-  let stack' = Hashtbl.create (module String) in
-  Hashtbl.iteri stack ~f:(fun ~key:k ~data:d ->
-    Hashtbl.update stack' k ~f:(fun _ -> Stack.copy d));
-  stack'
-
-let _rename_vars prog vars block_map cfg_succ dt =
+let rename_vars vars func_name func_args phi_map blocks cfg_succ dt =
+  let block_map = Hashtbl.of_alist_exn (module String) blocks in
   let stack = Hashtbl.create (module String) in
   List.iter vars ~f:(fun (v,_) ->
     let counter = Stdlib.Stream.from (fun i -> Some (i+1)) in
     Hashtbl.add_exn stack ~key:v ~data:((Stack.singleton v,counter)));
+  let func_args' = update_func_args stack func_args in 
   let rec rename block_name =
     let block = Hashtbl.find_exn block_map block_name in
+    let block_phis = Hashtbl.find_exn phi_map block_name in
+    Hashtbl.map_inplace block_phis ~f:(fun (Phi' (dst,typ,args)) ->
+      Phi' (process_dst stack dst, typ, args));
     let block', stack_hist = update_intrs stack block in
     Hashtbl.update block_map block_name ~f:(fun _ -> block');
     (* update phi-nodes *)
     let succs = Hashtbl.find_exn cfg_succ block_name in
     List.iter succs ~f:(fun succ_name ->
-      Hashtbl.update block_map succ_name ~f:(function 
-        | None -> failwith "Should not be reachable"
-        | Some block -> update_phi_reads stack block_name block));
+      let phis = Hashtbl.find_exn phi_map succ_name in
+      Hashtbl.mapi_inplace phis ~f:(fun ~key:phi ~data:(Phi' (dst, typ, args)) ->
+        let v' = process_arg stack phi in
+        Phi' (dst, typ, (block_name,v') :: args)));
     (* for b in children in dt *)
     let d_children = Hashtbl.find_exn dt block_name in
     Hash_set.iter d_children ~f:rename;
     List.iter stack_hist ~f:(fun v ->
       ignore (Hashtbl.find_exn stack v |> fst |> Stack.pop_exn))
   in
-  let prog' = List.map prog ~f:(fun func ->
-    let func' = { func with args = update_func_args stack func.args } in
-    rename func.name;
-    func')
-  in
-  prog', Hashtbl.to_alist block_map 
+  rename func_name;
+  func_args', block_map
 
-let to_ssa prog blocks cfg_succ df _dt =
-  let _vars, block_map = insert_nodes prog blocks df in
+(** TODO *)
+let _remove_bad_phis _phi_map _cfg_pred = ()
+
+let insert_phis phi_map block_map =
+  Hashtbl.mapi block_map ~f:(fun ~key:name ~data:block ->
+    let phis = Hashtbl.find_exn phi_map name
+      |> Hashtbl.to_alist
+      |> List.map ~f:(fun (_,(Phi' (dst,typ,args))) -> Phi (dst,typ,args))
+    in
+    match block with
+    | [] -> phis
+    | hd :: tl -> hd :: phis @ tl)
+
+let to_ssa prog blocks cfg_succ df dt =
+  let funcs, blocks = List.map prog ~f:(fun func -> 
+    let lbls = traverse_cfg_pre func.name cfg_succ |> Hash_set.of_list (module String) in
+    let blocks' = List.filter blocks ~f:(fun (lbl,_) -> Hash_set.mem lbls lbl) in
+    let func_args = match func.args with None -> [] | Some (args) -> args in
+    let vars, phi_map = insert_nodes func.name func_args blocks' df in
+    let func_args', block_map = rename_vars vars func.name func_args phi_map blocks' cfg_succ dt in
+    let block_map' = insert_phis phi_map block_map in
+    let func_args'' = if List.is_empty func_args' then None else Some (func_args') in
+    let func' = { func with args = func_args'' } in
+    let func'' = prog_from_block_map [func'] block_map' cfg_succ |> List.hd_exn in 
+    (func'', Hashtbl.to_alist block_map'))
+    |> List.unzip
+  in
+  funcs, List.concat blocks
+  (*let _vars, block_map = insert_nodes prog blocks df in
   (*let prog', blocks' = rename_vars prog vars block_map cfg_succ dt in
   prog_from_block_list prog' blocks' cfg_succ, blocks'*)
-  prog_from_block_map prog block_map cfg_succ, blocks
+  prog_from_block_map prog block_map cfg_succ, blocks*)
 
 (*let from_ssa prog blocks cfg_pred = 
   List.iter blocks ~f:(fun (lbl, instrs) -> 
