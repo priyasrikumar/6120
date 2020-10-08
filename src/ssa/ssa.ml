df1772d48f71ddfc92031f35dc184e47b3daab01
Goktug <gs2417@nyu.edu>
2020-10-08 19:15:48 -0400

    to_ssa and from_ssa should fully work

--- src/ssa/ssa.ml
+++ src/ssa/ssa.ml
@@ -67,50 +67,53 @@ let insert_nodes func_name func_args blocks df =
             Queue.enqueue defs_queue block
           end)
     done);
-  vars, phi_map
+  phi_map
 
 let mk_name v i = Printf.sprintf "%s_%d" v i
 
-let process_arg stack arg =
-  Hashtbl.find_exn stack arg |> fst |> Stack.top_exn
+let mk_counter var = Stdlib.Stream.from (fun i -> Some (mk_name var i))
 
-let process_dst stack dst =
-  let stack, stream = Hashtbl.find_exn stack dst in
-  let new_num = Stdlib.Stream.next stream in
-  let new_name = mk_name dst new_num in
-  Stack.push stack new_name;
+let process_arg stack counters arg =
+  Hashtbl.find_or_add stack arg ~default:(fun () ->
+    let counter = mk_counter arg in
+    let new_name = Stdlib.Stream.next counter in
+    Hashtbl.add_exn counters ~key:arg ~data:counter;
+    Stack.singleton new_name)
+  |> Stack.top_exn
+
+let process_dst stack counters dst =
+  let new_name = Hashtbl.find_or_add counters dst
+    ~default:(fun () -> mk_counter dst) |> Stdlib.Stream.next
+  in 
+  Hashtbl.update stack dst ~f:(function
+    | None -> Stack.singleton new_name
+    | Some stck -> Stack.push stck new_name; stck);
   new_name
 
-let update_intrs stack block =
-  let process_arg arg = process_arg stack arg in
-  let process_dst dst = process_dst stack dst in
-  let stack_hist = ref [] in 
+let update_intrs stack counters block =
+  let process_arg arg = process_arg stack counters arg in
+  let process_dst dst = process_dst stack counters dst in
   let block' = List.map block ~f:(fun instr ->
     match instr with
     | Label _ -> instr
     | Cst (dst, typ, cst) ->
-        stack_hist := dst :: !stack_hist;
         Cst (process_dst dst, typ, cst)
     | Binop (dst, typ, binop, arg1, arg2) ->
         let arg1', arg2' = process_arg arg1, process_arg arg2 in
-        stack_hist := dst :: !stack_hist;
         Binop (process_dst dst, typ, binop, arg1', arg2')
     | Unop (dst, typ, unop, arg) ->
         let arg' = process_arg arg in
-        stack_hist := dst :: !stack_hist;
         Unop (process_dst dst, typ, unop, arg')
     | Jmp _ -> instr
     | Br (arg, lbl1, lbl2) ->
         Br (process_arg arg, lbl1, lbl2)
     | Call (Some (dst), typ, name, Some (args)) ->
         let args' = List.map args ~f:process_arg in
-        stack_hist := dst :: !stack_hist;
         Call (Some (process_dst dst), typ, name, Some (args'))
     | Call (None, typ, name, Some (args)) ->
         let args' = List.map args ~f:process_arg in
         Call (None, typ, name, Some (args'))
     | Call (Some (dst), typ, name, None) ->
-        stack_hist := dst :: !stack_hist;
         Call (Some (process_dst dst), typ, name, None)
     | Call (None, _, _, None) -> instr
     | Ret (Some (arg)) ->
@@ -120,63 +123,74 @@ let update_intrs stack block =
         Print (List.map args ~f:process_arg)
     | Nop -> instr
     | Phi (dst, typ, phis) ->
-        stack_hist := dst :: !stack_hist;
         let phis' = phis (*List.map phis ~f:(fun (lbl,arg) -> (lbl,process_arg arg)) *)in
         Phi (process_dst dst, typ, phis'))
 in
-block', !stack_hist
+block'
 
-(*
-let update_phi_reads stack l_prev block =
-  let process_arg dst = process_arg stack dst in
-  List.map block ~f:(fun instr ->
-    match instr with
-    | Phi (dst, typ, phis) ->
-      let v' = process_arg dst in
-      let phis' = (l_prev,v') :: phis in 
-      Phi (dst, typ, phis')
-    | _ -> instr)
-*)
-
-(*let update_func_args stack args =
-  Option.(args >>| (fun args' ->
-    List.map args' ~f:(fun (arg,typ) ->
-      (process_arg stack arg,typ))))*)
-let update_func_args stack args =
+let _update_func_args stack args =
   List.map args ~f:(fun (arg,typ) -> (process_arg stack arg,typ))
 
-let rename_vars vars func_name func_args phi_map blocks cfg_succ dt =
+let copy_stack stack =
+  let stack' = Hashtbl.create (module String) in
+  Hashtbl.iteri stack ~f:(fun ~key:k ~data:d ->
+    Hashtbl.add_exn stack' ~key:k ~data:(Stack.copy d));
+  stack'
+
+let rename_vars func_args func_name phi_map blocks cfg_succ dt =
   let block_map = Hashtbl.of_alist_exn (module String) blocks in
   let stack = Hashtbl.create (module String) in
-  List.iter vars ~f:(fun (v,_) ->
-    let counter = Stdlib.Stream.from (fun i -> Some (i+1)) in
-    Hashtbl.add_exn stack ~key:v ~data:((Stack.singleton v,counter)));
-  let func_args' = update_func_args stack func_args in 
+  let counters = Hashtbl.create (module String) in
+  List.iter func_args ~f:(fun (v,_) ->
+    Hashtbl.add_exn stack ~key:v ~data:(Stack.singleton v);
+    Hashtbl.add_exn counters ~key:v ~data:(mk_counter v));
   let rec rename block_name =
+    let old_stack = copy_stack stack in 
     let block = Hashtbl.find_exn block_map block_name in
     let block_phis = Hashtbl.find_exn phi_map block_name in
     Hashtbl.map_inplace block_phis ~f:(fun (Phi' (dst,typ,args)) ->
-      Phi' (process_dst stack dst, typ, args));
-    let block', stack_hist = update_intrs stack block in
+      Phi' (process_dst stack counters dst, typ, args));
+    let block' = update_intrs stack counters block in
     Hashtbl.update block_map block_name ~f:(fun _ -> block');
     (* update phi-nodes *)
     let succs = Hashtbl.find_exn cfg_succ block_name in
     List.iter succs ~f:(fun succ_name ->
-      let phis = Hashtbl.find_exn phi_map succ_name in
-      Hashtbl.mapi_inplace phis ~f:(fun ~key:phi ~data:(Phi' (dst, typ, args)) ->
-        let v' = process_arg stack phi in
-        Phi' (dst, typ, (block_name,v') :: args)));
+      let succ_phis = Hashtbl.find_exn phi_map succ_name in
+      Hashtbl.mapi_inplace succ_phis ~f:(fun ~key:phi ~data:((Phi' (dst, typ, args)) as d) ->
+        if Hashtbl.mem stack phi then
+          let v' = process_arg stack counters phi in
+          Phi' (dst, typ, (block_name,v') :: args)
+        else
+          d));
     (* for b in children in dt *)
     let d_children = Hashtbl.find_exn dt block_name in
     Hash_set.iter d_children ~f:rename;
-    List.iter stack_hist ~f:(fun v ->
-      ignore (Hashtbl.find_exn stack v |> fst |> Stack.pop_exn))
+    Hashtbl.mapi_inplace stack ~f:(fun ~key:k ~data:d ->
+      Hashtbl.find_and_call old_stack k
+        ~if_found:(fun s -> Stack.copy s)
+        ~if_not_found:(fun _ -> d))
   in
   rename func_name;
-  func_args', block_map
+  block_map
 
-(** TODO *)
-let _remove_bad_phis _phi_map _cfg_pred = ()
+let remove_bad_phis phi_map cfg_pred =
+  let to_del = Hash_set.create (module String) in
+  let finish = ref false in
+  while !finish |> not do
+    finish := true;
+    Hashtbl.iteri phi_map ~f:(fun ~key:block ~data:phis ->
+      Hashtbl.iter phis ~f:(fun (Phi' (d,_t,args)) ->
+        let ok_args_len = List.fold_left args ~init:0
+          ~f:(fun acc (_,arg) -> if Hash_set.mem to_del arg then acc else acc + 1)
+        in
+        let preds_len = Hashtbl.find_exn cfg_pred block |> List.length in 
+        if ok_args_len < preds_len && Hash_set.mem to_del d |> not then begin
+          finish := false;
+          Hash_set.add to_del d
+        end))
+  done;
+  Hashtbl.map_inplace phi_map ~f:(fun phi ->
+    Hashtbl.filter phi ~f:(fun (Phi' (d,_,_)) -> Hash_set.mem to_del d |> not))
 
 let insert_phis phi_map block_map =
   Hashtbl.mapi block_map ~f:(fun ~key:name ~data:block ->
@@ -188,32 +202,51 @@ let insert_phis phi_map block_map =
     | [] -> phis
     | hd :: tl -> hd :: phis @ tl)
 
-let to_ssa prog blocks cfg_succ df dt =
+let to_ssa prog blocks cfg_succ cfg_pred df dt =
   let funcs, blocks = List.map prog ~f:(fun func -> 
     let lbls = traverse_cfg_pre func.name cfg_succ |> Hash_set.of_list (module String) in
     let blocks' = List.filter blocks ~f:(fun (lbl,_) -> Hash_set.mem lbls lbl) in
     let func_args = match func.args with None -> [] | Some (args) -> args in
-    let vars, phi_map = insert_nodes func.name func_args blocks' df in
-    let func_args', block_map = rename_vars vars func.name func_args phi_map blocks' cfg_succ dt in
+    let phi_map = insert_nodes func.name func_args blocks' df in
+    let block_map = rename_vars func_args func.name phi_map blocks' cfg_succ dt in
+    remove_bad_phis phi_map cfg_pred;
     let block_map' = insert_phis phi_map block_map in
-    let func_args'' = if List.is_empty func_args' then None else Some (func_args') in
-    let func' = { func with args = func_args'' } in
-    let func'' = prog_from_block_map [func'] block_map' cfg_succ |> List.hd_exn in 
-    (func'', Hashtbl.to_alist block_map'))
-    |> List.unzip
+    let func'' = prog_from_block_map [func] block_map' cfg_succ |> List.hd_exn in 
+    (func'', Hashtbl.to_alist block_map')) |> List.unzip
   in
   funcs, List.concat blocks
-  (*let _vars, block_map = insert_nodes prog blocks df in
-  (*let prog', blocks' = rename_vars prog vars block_map cfg_succ dt in
-  prog_from_block_list prog' blocks' cfg_succ, blocks'*)
-  prog_from_block_map prog block_map cfg_succ, blocks*)
 
-(*let from_ssa prog blocks cfg_pred = 
-  List.iter blocks ~f:(fun (lbl, instrs) -> 
-    List.iter instrs ~f:(function 
-    | Phi (dst, typ, lst) -> List.iter lst 
-    ~f:(fun (lbl, arg) -> 
-    let preds = Hashtbl.find_exn cfg_pred lbl in 
-      
-    )
-    | _ -> ()))*)
+let func_from_ssa blocks =
+  let block_map = Hashtbl.of_alist_exn (module String) blocks in
+  let add_copies dst typ args =
+    List.iter args ~f:(fun (lbl,arg) ->
+      let instr = Unop (dst, typ, Id, arg) in 
+      let new_instrs = 
+        match Hashtbl.find_exn block_map lbl |> List.rev with
+        | [] -> [instr]
+        | hd :: tl -> (hd :: instr :: tl) |> List.rev
+      in
+      Hashtbl.update block_map lbl ~f:(fun _ -> new_instrs))
+  in 
+  List.iter blocks ~f:(fun (_,blocks) ->
+    List.iter blocks ~f:(fun instr ->
+      match instr with
+      | Phi (dst, typ, args) -> add_copies dst typ args
+      | _ -> ()));
+  Hashtbl.map_inplace block_map ~f:(fun instrs -> 
+    List.filter instrs ~f:(fun instr ->
+      match instr with
+      | Phi (_,_,_) -> false
+      | _ -> true));
+  block_map
+
+
+let from_ssa prog blocks cfg_succ = 
+  let funcs, blocks = List.map prog ~f:(fun func ->
+    let lbls = traverse_cfg_pre func.name cfg_succ |> Hash_set.of_list (module String) in
+    let blocks' = List.filter blocks ~f:(fun (lbl,_) -> Hash_set.mem lbls lbl) in
+    let block_map = func_from_ssa blocks' in
+    let func' = prog_from_block_map [func] block_map cfg_succ |> List.hd_exn in
+    (func', Hashtbl.to_alist block_map)) |> List.unzip
+  in
+  funcs, List.concat blocks
--- src/ssa/ssa.mli
+++ src/ssa/ssa.mli
@@ -1,2 +1,5 @@
-val to_ssa : Types.prog -> Cfg.blocks_t -> Cfg.cfg_t -> Cfg.df_t -> Cfg.dt_t
+val to_ssa : Types.prog -> Cfg.blocks_t -> Cfg.cfg_t -> Cfg.cfg_t -> Cfg.df_t -> Cfg.dt_t
+  -> Types.prog * Cfg.blocks_t
+
+val from_ssa : Types.prog -> Cfg.blocks_t -> Cfg.cfg_t
   -> Types.prog * Cfg.blocks_t
