6f12986539af31b8b95847caf6bf8cea792a333c
Goktug <gs2417@nyu.edu>
2020-12-06 20:23:43 -0500

    lifting dummy fix

--- src/hof/lifting.ml
+++ src/hof/lifting.ml
@@ -1,9 +1,11 @@
+(*
 open Types 
 open Core 
+*)
 
 let free_vars = () 
 
 let process_anon = ()
 
-let process_rest cfg = () (* List.map cfg ~f:(fun cfg_func -> 
+let process_rest _cfg = () (* List.map cfg ~f:(fun cfg_func -> 
     let rec ) *)
